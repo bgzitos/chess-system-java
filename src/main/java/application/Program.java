eb40a9dc6d6e9ea6acdf2357f6f5397365aafeac
bgzitos <lucaslopes49441008@gmail.com>
2026-04-07 23:26:10 -0300

    Position Class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     void main() {
-        
-        System.out.println("Hello world!");
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
